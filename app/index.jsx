9de43dcdf41517e13d548ee7efca044d2a321b95
SaiRakindi <sai.rakindi3@gmail.com>
2024-04-19 15:25:44 +0530

    feat: tailwind configuration

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,17 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
+    <View className="flex-1 items-center justify-center bg-red">
+      <Text className="text-3xl">Aora!</Text>
+
       <StatusBar style="auto" />
+
+      <Link href="/profile" style={{ color: "blue" }}>
+        Profile
+      </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
