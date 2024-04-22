75a7a1915af4e3074c008485811df692eba14ccb
SaiRakindi <sai.rakindi3@gmail.com>
2024-04-22 22:49:48 +0530

    feat: added sign in link using expo router on onboarding screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -41,7 +41,9 @@ export default function App() {
 
           <CustomButton
             title="Continue with Email"
-            handlePress={() => {}}
+            handlePress={() => {
+              router.push("/sign-in");
+            }}
             containerStyles={"w-full mt-7"}
           />
         </View>
