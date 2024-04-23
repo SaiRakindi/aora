ea5bbe8185e5a3b7d40d3344b9468cad06e5b872
SaiRakindi <sai.rakindi3@gmail.com>
2024-04-23 23:26:35 +0530

    feat: sign in form field reusable component

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,9 +1,48 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+
 const SignIn = () => {
-  return <SafeAreaView className="bg-primary h-full"></SafeAreaView>;
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(event) => setForm({ ...form, email: event })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(event) => setForm({ ...form, password: event })}
+            otherStyles="mt-7"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default SignIn;
